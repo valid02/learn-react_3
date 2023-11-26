8e26848f02c228e84b4e23ebe7b729d0d4f7fd9d
Valid Rabiee <validrabieejs@gmail.com>
2023-11-26 22:04:49 +0330

    memo in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import Button from "./components/UI/Button/Button";
 import './App.css';
+import DemoOutput from "./components/Demo/DemoOutput";
 
-function App() {
+const App =() => {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  console.log('APP RUNNING');
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hello World</h1>
+      <DemoOutput show={false}/>
+      <Button onClick={toggleParagraphHandler}>show paragraph</Button>
     </div>
   );
 }
